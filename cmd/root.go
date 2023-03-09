cc7e97323f4060833b0454b7cd35c60ab962a1ca
santiago <santiago@bankuish.com>
2023-03-09 03:16:54 -0300

    Futures command added

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,14 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/seorlando33/binance-data-retriever/cmd/futures"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,6 +23,10 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func addSubCommands() {
+	rootCmd.AddCommand(futures.FuturesCmd)
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -46,6 +48,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	addSubCommands()
 }
 
 
